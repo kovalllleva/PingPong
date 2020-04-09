d1f128a495f5cc6e16d78ddcc1c8d46538b3af76
motyl <kovalevao928@gmail.com>
2020-04-09 21:36:46 +0400

    Добавьте файлы проекта.

---
+++ PingPong/Form1.Designer.cs
@@ -0,0 +1,31 @@
+﻿namespace PingPong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        
+
+        private System.Windows.Forms.Panel background;
+        private System.Windows.Forms.PictureBox gameBal;
+        private System.Windows.Forms.PictureBox gamePanel;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ PingPong/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PingPong
+{
+    public partial class Form1 : Form
+    {
+
+        private int speed_vertical = 4;
+        private int speed_hor = 4;
+        private int score = 0;
+           
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer.Enabled = true;
+
+            Cursor.Hide();
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.TopMost = true;
+
+            this.Bounds = Screen.PrimaryScreen.Bounds;
+
+            gamePanel.Top = background.Bottom - (background.Bottom / 10);
+
+        }
+
+        
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+                this.Close();
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(560, 261);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
